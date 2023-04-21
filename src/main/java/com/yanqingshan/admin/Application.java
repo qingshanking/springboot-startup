2d6b858c265a5c4a368e3585a049ece512c4485c
qingshanking <qingshanking@vip.qq.com>
2023-04-21 18:05:54 +0800

    init:初始化框架

---
+++ src/main/java/com/yanqingshan/admin/Application.java
@@ -0,0 +1,17 @@
+package com.yanqingshan.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author yanqs
+ * @date 2023年04月21日 17:54
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
