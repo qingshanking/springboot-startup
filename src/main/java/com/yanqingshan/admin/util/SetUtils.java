e330d5a444153fd995a02299b09543608ad8a2ff
qingshanking <qingshanking@vip.qq.com>
2023-04-21 18:34:46 +0800

    init:框架增加项目修改器测试方法和工具依赖包

---
+++ src/main/java/com/yanqingshan/admin/util/SetUtils.java
@@ -0,0 +1,16 @@
+package com.yanqingshan.admin.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author yanqs
+ * @date 2023年04月21日 18:24
+ */
+public class SetUtils {
+    @SafeVarargs
+    public static <T> Set<T> asSet(T... objs) {
+        return new HashSet<>(Arrays.asList(objs));
+    }
+}
