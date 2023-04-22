f3bc88d583aee4d1e698637f65acb27e979f3852
qingshanking <qingshanking@vip.qq.com>
2023-04-22 14:42:12 +0800

    feat:集成knife4j并编写测试接口

--- src/main/java/com/yanqingshan/admin/module/test/controller/TestController.java
+++ src/main/java/com/yanqingshan/admin/module/test/controller/TestController.java
@@ -1,5 +1,9 @@
-package com.yanqingshan.admin.controller;
+package com.yanqingshan.admin.module.test.controller;
 
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import com.github.xiaoymin.knife4j.annotations.ApiSupport;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yanqs
  * @date 2023年04月21日 18:37
  */
+@Api(tags = "测试接口")
+@ApiSupport(order = 1)
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @ApiOperationSupport(order = 1)
+    @ApiOperation(value = "输出文字")
     @GetMapping(value = "/say")
     public String say() {
         return "Hello SpringBoot Startup";
