4794f77454135f15f0d96c51e2ef0da4326157bc
qingshanking <22401257+qingshanking@users.noreply.github.com>
2023-05-05 17:44:13 +0800

    feat:增加应用模块常量

---
+++ src/main/java/com/yanqingshan/admin/common/constant/AppConstant.java
@@ -0,0 +1,24 @@
+package com.yanqingshan.admin.common.constant;
+
+/**
+ * App 接口前缀接口名称
+ *
+ * @author yanqs
+ * @date 2023年05月05日 17:38
+ */
+public interface AppConstant {
+
+    /**
+     * 认证模块
+     */
+    String APPLICATION_AUTH = "auth";
+    /**
+     * 系统模块
+     */
+    String APPLICATION_SYSTEM = "system";
+
+    /**
+     * 测试模块
+     */
+    String APPLICATION_TEST = "test";
+}
